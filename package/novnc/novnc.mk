1f53ee5b8e3b61262abf0e4738f5a77f9e81f0da
David Kerr <david@kerr.net>
2017-09-09 20:21:14 -0400

    Add target install rules to novnc make file

--- package/novnc/novnc.mk
+++ package/novnc/novnc.mk
@@ -8,19 +8,20 @@ NOVNC_VERSION = master
 NOVNC_SOURCE = $(NOVNC_VERSION).tar.gz
 NOVNC_SITE = https://github.com/novnc/noVNC/archive
 NOVNC_LICENSE = MPL-2.0 and LGPL-3.0
-NOVNC_CONFIGURE_CMDS = "echo"
-NOVNC_BUILD_CMDS = "echo"
+NOVNC_CONFIGURE_CMDS = echo
+NOVNC_BUILD_CMDS = echo
 
 NOVNC_TARGET_DIR=stat/var/www/novnc
 
-novnc-install:
-	echo "here"
+define NOVNC_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/$(NOVNC_TARGET_DIR)
+	cp $(@D)/*.html $(TARGET_DIR)/$(NOVNC_TARGET_DIR)
 	mkdir -p $(TARGET_DIR)/$(NOVNC_TARGET_DIR)/core
+	cp -r $(@D)/core $(TARGET_DIR)/$(NOVNC_TARGET_DIR)
 	mkdir -p $(TARGET_DIR)/$(NOVNC_TARGET_DIR)/app
-
-
-novnc: novnc-install
-
+	cp -r $(@D)/app $(TARGET_DIR)/$(NOVNC_TARGET_DIR)
+	mkdir -p $(TARGET_DIR)/$(NOVNC_TARGET_DIR)/vendor
+	cp -r $(@D)/vendor $(TARGET_DIR)/$(NOVNC_TARGET_DIR)
+endef
 
 $(eval $(call AUTOTARGETS,package,novnc))
