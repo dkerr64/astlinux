455972cf866b437d86b8dc84dfad2e57bf3fd1e8
David Kerr <david@kerr.net>
2017-10-03 09:16:55 -0400

    Make /etc/ssl/certs director if it doesn't exist, else build fails.

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -16,11 +16,13 @@ define CA_CERTIFICATES_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/lib/ssl/certs
 	$(INSTALL) -m 0444 -D $(@D)/cacert.pem $(TARGET_DIR)/usr/share/ca-certificates/ca-bundle.crt
 	ln -sf /usr/share/ca-certificates/ca-bundle.crt $(TARGET_DIR)/usr/lib/ssl/certs/ca-bundle.crt
+	mkdir -p $(TARGET_DIR)/etc/ssl/certs
 	ln -sf /usr/share/ca-certificates/ca-bundle.crt $(TARGET_DIR)/etc/ssl/certs/ca-certificates.crt
 endef
 
 define CA_CERTIFICATES_UNINSTALL_TARGET_CMDS
   rm -f  $(TARGET_DIR)/etc/ssl/certs/ca-certificates.crt
+  rmdir --ignore-fail-on-non-empty $(TARGET_DIR)/etc/ssl/certs
 	rm -f  $(TARGET_DIR)/usr/lib/ssl/certs/ca-bundle.crt
 	rm -rf $(TARGET_DIR)/usr/share/ca-certificates
 endef
