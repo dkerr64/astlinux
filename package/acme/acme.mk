282c326ec8e308c07a6b293d3f480cdbd0159575
David Kerr <david@kerr.net>
2018-04-15 21:31:50 -0400

    Acme version bump to 2.7.8

--- package/acme/acme.mk
+++ package/acme/acme.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ACME_VERSION = 2.7.2
+ACME_VERSION = 2.7.8
 ACME_SOURCE = acme.sh-$(ACME_VERSION).tar.gz
 ACME_SITE = https://s3.amazonaws.com/files.astlinux-project
 
@@ -18,13 +18,11 @@ ACME_REMOVE_TARGET_DNSAPI = dns_myapi.sh dns_lexicon.sh $(if $(BR2_PACKAGE_BIND)
 define ACME_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 package/acme/deploy/astlinux.sh $(TARGET_DIR)/stat/etc/acme/deploy/astlinux.sh
 	$(INSTALL) -D -m 0644 package/acme/deploy/custom.sh $(TARGET_DIR)/stat/etc/acme/deploy/custom.sh
-	$(INSTALL) -D -m 0644 package/acme/deploy/ssh.sh $(TARGET_DIR)/stat/etc/acme/deploy/ssh.sh
-	$(INSTALL) -D -m 0644 package/acme/dnsapi/dns_dyn.sh $(TARGET_DIR)/stat/etc/acme/dnsapi/dns_dyn.sh
-	$(INSTALL) -D -m 0644 package/acme/dnsapi/dns_duckdns.sh $(TARGET_DIR)/stat/etc/acme/dnsapi/dns_duckdns.sh
+	$(INSTALL) -D -m 0644 $(@D)/deploy/ssh.sh $(TARGET_DIR)/stat/etc/acme/deploy/ssh.sh
 	$(INSTALL) -D -m 0755 package/acme/acme-client.sh $(TARGET_DIR)/usr/sbin/acme-client
 	$(INSTALL) -D -m 0755 $(@D)/acme.sh $(TARGET_DIR)/stat/etc/acme/acme.sh
 	cp -a $(@D)/dnsapi $(TARGET_DIR)/stat/etc/acme/
-	ln -s /mnt/kd/acme $(TARGET_DIR)/etc/acme
+	ln -s -f /mnt/kd/acme $(TARGET_DIR)/etc/acme
 	# Remove non-required dnsapi files
 	rm -f $(addprefix $(TARGET_DIR)/stat/etc/acme/dnsapi/, $(ACME_REMOVE_TARGET_DNSAPI))
 	# Make the dnsapi scripts non-executable, they are sourced by acme.sh
